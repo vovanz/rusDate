e4a9aeacd26b5a55cc48fc948ef5e5c64f68a5ca
unknown <C:\Users\VovanZ\AppData\Roaming\The Bat!>
2012-10-18 22:26:53 +0600

    test

---
+++ CRusDate.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Вспомогательный класс для русской версии функции date 
+ * @author VovanZ
+ */
+class CRusDate extends CApplicationComponent {
+
+    /**
+     * Массив пар для замены
+     * @var array 
+     */
+    public $translate = array(
+        "am" => "дп",
+        "pm" => "пп",
+        "AM" => "ДП",
+        "PM" => "ПП",
+        "Monday" => "Понедельник",
+        "Mon" => "Пн",
+        "Tuesday" => "Вторник",
+        "Tue" => "Вт",
+        "Wednesday" => "Среда",
+        "Wed" => "Ср",
+        "Thursday" => "Четверг",
+        "Thu" => "Чт",
+        "Friday" => "Пятница",
+        "Fri" => "Пт",
+        "Saturday" => "Суббота",
+        "Sat" => "Сб",
+        "Sunday" => "Воскресенье",
+        "Sun" => "Вс",
+        "January" => "Января",
+        "Jan" => "Янв",
+        "February" => "Февраля",
+        "Feb" => "Фев",
+        "March" => "Марта",
+        "Mar" => "Мар",
+        "April" => "Апреля",
+        "Apr" => "Апр",
+        "May" => "Мая",
+        "May" => "Мая",
+        "June" => "Июня",
+        "Jun" => "Июн",
+        "July" => "Июля",
+        "Jul" => "Июл",
+        "August" => "Августа",
+        "Aug" => "Авг",
+        "September" => "Сентября",
+        "Sep" => "Сен",
+        "October" => "Октября",
+        "Oct" => "Окт",
+        "November" => "Ноября",
+        "Nov" => "Ноя",
+        "December" => "Декабря",
+        "Dec" => "Дек",
+        "st" => "ое",
+        "nd" => "ое",
+        "rd" => "е",
+        "th" => "ое"
+    );
+
+    /**
+     * Вызов функции date и замена английский слов на русские. Использование аналогично встроенной функции date.
+     * @return string 
+     */
+    public function date() {
+        if (func_num_args() > 1) {
+            $timestamp = func_get_arg(1);
+            return strtr(date(func_get_arg(0), $timestamp), $this->translate);
+        } else {
+            return strtr(date(func_get_arg(0)), $this->translate);
+        }
+    }
+
+}
